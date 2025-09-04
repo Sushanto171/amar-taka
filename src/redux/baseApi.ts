ed52dfea9194d4e69d2041ccc6593ac1369dea04
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-09-05 00:42:05 +0600

    configure redux RTK query

---
+++ src/redux/baseApi.ts
@@ -0,0 +1,9 @@
+import { config } from "@/config";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: config.baseUrl }),
+  endpoints: () => ({}),
+  tagTypes: ["USER"],
+});
---
+++ src/redux/store.ts
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./baseApi";
+export const store = configureStore({
+  reducer: { [baseApi.reducerPath]: baseApi.reducer },
+});
